4df179ae8317869dd3665700957f70655d0cd848
Githubpoozas <po.oz.as@hotmail.com>
2020-02-03 00:56:20 +0700

    switch to twentytwenty add footer, remove search from  header,add css

--- wp-content/themes/twentytwenty/footer.php
+++ wp-content/themes/twentytwenty/footer.php
@@ -12,48 +12,101 @@
  */
 
 ?>
-			<footer id="site-footer" role="contentinfo" class="header-footer-group">
 
-				<div class="section-inner">
+<!-- social media bar -->
+<div class="socialmedia d-none d-md-block" id="socialmedia">
+  <input
+	type="checkbox"
+	class="socialmedia__checkbox"
+	id="socialmedia-toggle"
+  />
+  <label for="socialmedia-toggle" class="socialmedia__button">
+	<div class="socialmedia__icon">
+	  <img src="../wp-content/themes/noproblem/library/images/socialmedia/arrow.svg" alt="" />
+	</div>
+  </label>
+  <nav class="socialmedia__nav">
+	<ul class="socialmedia__list">
+	  <li class="socialmedia__item">
+		<a href="https://wa.me/66817741166" target="_blank"><img class="btn-shadow" src="../wp-content/themes/noproblem/library/images/socialmedia/whatsapp.svg" alt=""/></a>
+	  </li>
+	  <li class="socialmedia__item">
+		<a href="https://www.facebook.com/130047820416542/" target="_blank">
+		  <img class="btn-shadow" src="../wp-content/themes/noproblem/library/images/socialmedia/facebook.svg" alt=""/>
+		</a>
+	  </li>
+	  <li class="socialmedia__item">
+		<a href="http://line.me/ti/p/~nicknoproblem" target="_blank"><img class="btn-shadow" src="../wp-content/themes/noproblem/library/images/socialmedia/line.svg" alt=""/></a>
+	  </li>
+	  <li class="socialmedia__item">
+		<a href="https://m.me/Noproblemtshirt/" target="_blank"><img class="btn-shadow" src="../wp-content/themes/noproblem/library/images/socialmedia/messenger.svg" alt=""/></a>
+	  </li>
+	  <li class="socialmedia__item">
+		<a href="#wechat_bottom"><img class="btn-shadow wechat" src="../wp-content/themes/noproblem/library/images/socialmedia/wechat.svg" alt=""/></a>
+	  </li>
+	</ul>
+  </nav>
+  <label class="socialmedia__rotated" lang="th" for="socialmedia-toggle"
+	>ติดต่อสอบถามได้ที่</label
+  >
+</div>
 
-					<div class="footer-credits">
+<!-- footer -->
+<footer class="footer" lang="th">
+  <div class="footer__box">
+	<div class="footer__logo">
+	  <img src="../wp-content/themes/noproblem/library/images/noproblem_logo.svg" alt="" />
+	</div>
+	<div class="footer__contacts">
+	  <div class="footer__contact footer__contact-1" lang="en">
+		<p class="footer__name">CONTACT</p>
+	  </div>
+	  <div class="footer__contact footer__contact-2">
+		<p>
+		  เลขที่ 127 ซ.สุขสวัสดิ์ 14/19 แขวงจอมทอง เขตจอมทอง กทม 10150
+		</p>
+	  </div>
+	  <div class="footer__contact footer__contact-3">
+		<p>โทรศัพท์</p>
+		<p><a href="tel:066-165-1987">0-2460-2200-11</a></p>
+		<p>แฟกซ์</p>
+		<p><a href="tel:024761866">0-2476-1866</a></p>
+	  </div>
+	  <div class="footer__contact footer__contact-4">
+		<p>เวลาทำการ จันทร์-เสาร์ เวลา 08:00-17:00</p>
+	  </div>
+	</div>
+	<div class="footer__socials">
+	  <div class="footer__social footer__social-1" lang="en">
+		<p class="footer__name">SOCIAL MEDIA</p>
+	  </div>
+	  <div class="footer__social footer__social-2">
+		<a href="https://wa.me/66817741166" target="_blank"><img class="btn-shadow" src="../wp-content/themes/noproblem/library/images/socialmedia/whatsapp.svg" alt=""/></a>
+		<a href="https://www.facebook.com/130047820416542/" target="_blank">
+		  <img class="btn-shadow" src="../wp-content/themes/noproblem/library/images/socialmedia/facebook.svg" alt=""/>
+		</a>
+		<a href="http://line.me/ti/p/~nicknoproblem" target="_blank"><img class="btn-shadow" src="../wp-content/themes/noproblem/library/images/socialmedia/line.svg" alt=""/></a>
+		<a href="https://m.me/Noproblemtshirt/" target="_blank"><img class="btn-shadow" src="../wp-content/themes/noproblem/library/images/socialmedia/messenger.svg" alt=""/></a>
 
-						<p class="footer-copyright">&copy;
-							<?php
-							echo date_i18n(
-								/* translators: Copyright date format, see https://secure.php.net/date */
-								_x( 'Y', 'copyright date format', 'twentytwenty' )
-							);
-							?>
-							<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
-						</p><!-- .footer-copyright -->
+	  </div>
+	  <div class="footer__social footer__social-wechat">
+		<img id="wechat_bottom" class="btn-shadow wechat" src="../wp-content/themes/noproblem/library/images/socialmedia/wechat.svg" alt=""/>
+		<p id="wechatId">ID: nicknoproblem</p>
+	  </div>
 
-						<p class="powered-by-wordpress">
-							<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentytwenty' ) ); ?>">
-								<?php _e( 'Powered by WordPress', 'twentytwenty' ); ?>
-							</a>
-						</p><!-- .powered-by-wordpress -->
+	  <div class="footer__social footer__social-3">
+		<p>CALL</p>
+		<p><a href="tel:0817741166">081-774-1166</a></p>
+	  </div>
+	  <div class="footer__social footer__social-4">
+		<p>EMAIL</p>
+		<p><a href="mailto:nicknoproblem@gmail.com">nicknoproblem@gmail.com</a></p>
+	  </div>
+	</div>
+  </div>
+</footer>
 
-					</div><!-- .footer-credits -->
-
-					<a class="to-the-top" href="#site-header">
-						<span class="to-the-top-long">
-							<?php
-							/* translators: %s: HTML character for up arrow */
-							printf( __( 'To the top %s', 'twentytwenty' ), '<span class="arrow" aria-hidden="true">&uarr;</span>' );
-							?>
-						</span><!-- .to-the-top-long -->
-						<span class="to-the-top-short">
-							<?php
-							/* translators: %s: HTML character for up arrow */
-							printf( __( 'Up %s', 'twentytwenty' ), '<span class="arrow" aria-hidden="true">&uarr;</span>' );
-							?>
-						</span><!-- .to-the-top-short -->
-					</a><!-- .to-the-top -->
-
-				</div><!-- .section-inner -->
-
-			</footer><!-- #site-footer -->
+<div class="footer__whitebar"></div>
 
 		<?php wp_footer(); ?>
 
